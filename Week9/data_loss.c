6793a493431e4226e0f75b351002dc60d72f1fda
alari409 <f1120726@gmail.com>
2025-11-04 16:58:08 +0900

    good afternoon

---
+++ Week9/data_loss.c
@@ -0,0 +1,23 @@
+﻿//data_loss.c
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned int x = 0x12346578;
+	long lg = 2147483648; //long의 최대값
+	double xl = 123456789123.456789;
+
+	printf("\nUint: %d\n", x);
+	printf("Uint -> short: %d\n", (short)x);
+	printf("Uint -> char: %d\n", (char)x);
+
+	printf("\nLong: %d\n", lg);
+	printf("Long -> int: %d\n", (int)lg);
+	printf("Long -> char: %d\n", (char)lg);
+
+	printf("\nDbl: %lf\n", xl);
+	printf("Dbl -> long: %d\n", (long)x);
+	printf("Dbl -> short: %d\n", (short)x);
+
+	return 0;
+}
---
+++ Week9/endian.c
@@ -0,0 +1,37 @@
+﻿//endian.c
+#include <stdio.h>
+
+void check_endian()
+{
+	//첫 메모리 주소가 0x12이면 빅 엔디언이고
+	//0x78이면 리틀 엔리언
+
+	unsigned int num = 0x13245678; // 16진법 (4바이트)
+	unsigned char* byte_ptr = (unsigned char*)&num;
+
+	//모든 바이트 주소와 값을 출력하기
+	for (int i = 0; i < sizeof(num); i++)
+	{
+		printf("%p   0x%02x\n", (byte_ptr + i), byte_ptr[i]);
+	}
+
+	//첫 바이트 주소를 확인하고 엔디언니스 출력하기
+	if (byte_ptr[0] == 0x12)
+	{
+		printf("\nThis is BIG-endian!");
+	}
+	else if (byte_ptr[0] == 0x78)
+	{
+		printf("\n This is little endian!..");
+	}
+	else
+	{
+		printf("\nunknown endian!..");
+	}
+}
+
+int main(void)
+{
+	check_endian();
+	return 0;
+}
